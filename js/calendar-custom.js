23fc7327cfbace29f2e041b9fa8e5797052f39e6
Salvador Mósca <brduca@gmail.com>
2016-08-10 22:21:51 +0100

    js calendar prototype

---
+++ js/calendar-custom.js
@@ -0,0 +1,25 @@
+﻿// Lookup for td with events
+$(".rsAptContent").closest("td").each(function () {
+
+    // Find events on td
+    var eventsByTd = $(this).find(".rsAptContent");
+    var header = $(this).find(".rsDateBox");
+    
+    for (var i = 0; i < eventsByTd.length; i++)
+    {
+        // Gets the color of the btn
+        var color = $(eventsByTd[i]).closest(".rsAptSimple").css("background-color");
+        
+        $("<span/>", {
+            width: 5,
+            height: 5,
+            css: {
+                backgroundColor: color
+            },
+            text: "*"
+        }).appendTo(header);
+        
+        
+    }
+
+});
